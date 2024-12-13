6c31ef02376bde11c3cb2513354f1c7f5ab8495b
Jahid Uddin <jahidudd65@gmail.com>
2024-12-13 16:25:40 +0100

    [update]: add basic http server

--- api.go
+++ api.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type ApiServer struct {
+	port uint
+}
+
+func NewApiServer(port uint) *ApiServer {
+	return &ApiServer{port: port}
+}
+
+func (s *ApiServer) Start() {
+	port := strconv.FormatUint(uint64(s.port), 10)
+	fmt.Printf("Server is running on http://localhost:%s\n", port)
+	err := http.ListenAndServe("localhost:"+port, nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
